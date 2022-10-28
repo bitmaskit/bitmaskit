352a3a8eaf29b8dc512d6f33ccda2854317ec1b8
Valentin <16714035+bitmaskit@users.noreply.github.com>
2022-10-28 16:19:10 +0300

    upd

--- .github/workflows/lib/octokit-plugin-check-follower-count.js
+++ .github/workflows/lib/octokit-plugin-check-follower-count.js
@@ -1,8 +1,8 @@
-module.exports = { checkFollowerCount };
+module.exports = {checkFollowerCount};
 
-const { updateFollowerCount } = require("./updateFollowers");
+const {updateFollowerCount} = require("./updateFollowers");
 const {
-  composeCreateOrUpdateTextFile,
+	composeCreateOrUpdateTextFile,
 } = require("@octokit/plugin-create-or-update-text-file");
 
 const owner = "bitmaskit";
@@ -12,39 +12,39 @@ const repo = "bitmaskit";
  * @param {import("octokit").Octokit} octokit
  */
 function checkFollowerCount(octokit) {
-  return {
-    async checkFollowerCount() {
-      try {
-        // fetch followers
-        const count = await octokit.request('GET /users/bitmaskit');
+	return {
+		async checkFollowerCount() {
+			try {
+				// fetch followers
+				const count = await octokit.request('GET /users/bitmaskit');
 
-        // try to update the README directly.
-        const update = await composeCreateOrUpdateTextFile(octokit, {
-          owner,
-          repo,
-          path: "README.md",
-          message: "Followers updated",
-          content: ({ content }) => {
-            return updateFollowerCount(content, count.data.followers);
-          },
-        });
-        console.log(update)
-      } catch (error) {
-        // if it fails, try to create an issue
-        const { data: issue } = await octokit
-          console.log(error)
-         .request("POST /repos/{owner}/{repo}/issues", {
-            owner,
-            repo,
-            title: "The .github/workflows/followers.yml did not work.",
-            body: `This didn't work directly, so here's an issue: ${error}`,
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+				// try to update the README directly.
+				const update = await composeCreateOrUpdateTextFile(octokit, {
+					owner,
+					repo,
+					path: "README.md",
+					message: "Followers updated",
+					content: ({content}) => {
+						return updateFollowerCount(content, count.data.followers);
+					},
+				});
+				console.log(update);
+			} catch (error) {
+				// if it fails, try to create an issue
+				const {data: issue} = await octokit;
+				console.log(error)
+					.request("POST /repos/{owner}/{repo}/issues", {
+						owner,
+						repo,
+						title: "The .github/workflows/followers.yml did not work.",
+						body: `This didn't work directly, so here's an issue: ${error}`,
+					})
+					.catch((err) => {
+						console.log(err);
+					});
 
-        console.log(`issue created at ${issue.html_url}`);
-      }
-    },
-  };
+				console.log(`issue created at ${issue.html_url}`);
+			}
+		},
+	};
 }
